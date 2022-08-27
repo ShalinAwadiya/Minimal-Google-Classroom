76a7d1589ee3fa0c42492b2da59ead1442572972
Shalin Hasanbhai Awadiya <awadiyashalin03@gmail.com>
2022-08-27 00:43:54 -0300

    Project Structure

---
+++ project/com/controller/__init__.py
@@ -0,0 +1,11 @@
+
+
+
+
+
+import project.com.controller.LoginController
+import project.com.controller.MainController
+import project.com.controller.StudentController
+import project.com.controller.SubjectController
+import project.com.controller.AssignmentController
+
